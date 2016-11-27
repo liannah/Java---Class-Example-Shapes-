f6df505cacd27001274fdc7c34777645671cb9c9
liannah <liannahovhannisyan7@gmail.com>
2016-11-27 21:39:58 +0400

    first commit

--- src/utilis/Math.java
+++ src/utilis/Math.java
@@ -4,14 +4,14 @@ public class Math {
 
 	public Math() {
 		}
- public static int factorial(int num){
+ public static long factorial(int num){
 	 if(num==0)
 		 return 1;
 	 else
 	 return num*factorial(num-1);
 	 }
- public static int factorialLoop(int value){
-	 int result=1;
+ public static long factorialLoop(int value){
+	 long result=1;
 	 for(int i=1;i<=value;i++){
 		 result = result*i;
 	 }
